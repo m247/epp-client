b7dcbea9d36b0d35f434a4fb1b849b5a10b21130
Geoff Garside <geoff@geoffgarside.co.uk>
2010-05-24 09:13:07 +0100

    Reorganise layout of response methods

--- lib/epp-client/response.rb
+++ lib/epp-client/response.rb
@@ -5,15 +5,30 @@ module EPP
       @xml.root.namespaces.default_prefix = 'e'
     end
 
-    def xml
-      @xml.to_s(:indent => true)
+    def success?
+      code == 1000
     end
+
     def code
       @code ||= result['code'].to_i
     end
     def message
       @message ||= result.find('e:msg/text()').first.content.strip
     end
+    def data
+      @data ||= @xml.find('/e:epp/e:response/e:resData/node()')[1]
+    end
+    def msgQ
+      @msgQ ||= @xml.find('/e:epp/e:response/e:msgQ').first
+    end
+
+    def inspect
+      @xml.inspect
+    end
+    def to_xml
+      @xml.to_s(:indent => true)
+    end
+
     private
       def result
         @result ||= @xml.find('/e:epp/e:response/e:result').first
