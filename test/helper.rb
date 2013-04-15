4b15b6c0755a4a2def5bf9a1567857bffdfbbe5f
Geoff Garside <geoff@geoffgarside.co.uk>
2013-04-15 12:01:20 +0100

    Add some tests for EPP::Server

--- test/helper.rb
+++ test/helper.rb
@@ -10,6 +10,13 @@ require 'epp-client'
 EPP::Request.enable_validation!
 
 class Test::Unit::TestCase
+  def stub_getaddrinfo!(host = 'epp.test.host', port = 700)
+    host ||= 'epp.test.host'
+    port ||= 700
+
+    addrinfo = [["AF_INET", port, "198.51.100.53", "198.51.100.53", 2, 1, 6]]
+    Socket.expects(:getaddrinfo).with(host, port, nil, Socket::SOCK_STREAM).returns(addrinfo).at_least_once
+  end
   def load_schema(name)
     xsd_path = File.expand_path("../schemas/#{name}.xsd", __FILE__)
     xsd_doc  = XML::Document.file(xsd_path)
---
+++ test/test_server.rb
@@ -0,0 +1,52 @@
+require 'helper'
+
+class TestEppServer < Test::Unit::TestCase
+  context 'EPP::Server' do
+    setup do
+      stub_getaddrinfo!
+
+      @server = EPP::Server.new('TEST', 'test', 'epp.test.host')
+    end
+
+    should 'be instance of EPP::Client' do
+      assert @server.kind_of?(EPP::Server)
+    end
+
+    should 'allow configuration of port' do
+      stub_getaddrinfo!(nil, 7001)
+
+      server = nil
+      assert_nothing_raised do
+        server = EPP::Server.new('TEST', 'test', 'epp.test.host', :port => 7001)
+      end
+
+      assert_equal 7001, server.options[:port]
+    end
+
+    should 'allow configuration of language' do
+      server = EPP::Server.new('TEST', 'test', 'epp.test.host', :lang => 'no')
+
+      assert_equal 'no', server.options[:lang]
+    end
+
+    should 'allow configuration of version' do
+      server = EPP::Server.new('TEST', 'test', 'epp.test.host', :version => '2.0')
+
+      assert_equal '2.0', server.options[:version]
+    end
+
+    should 'allow configuration of extensions' do
+      extensions = %w(urn:ietf:params:xml:ns:secDNS-1.1)
+      server = EPP::Server.new('TEST', 'test', 'epp.test.host', :extensions => extensions)
+
+      assert_equal extensions, server.options[:extensions]
+    end
+
+    should 'allow configuration of services' do
+      services = %w(http://www.nominet.org.uk/epp/xml/nom-domain-2.0)
+      server = EPP::Server.new('TEST', 'test', 'epp.test.host', :services => services)
+
+      assert_equal services, server.options[:services]
+    end
+  end
+end
