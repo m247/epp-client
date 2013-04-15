4d0227388258b72f47290c87ba0936812bb614e4
Geoff Garside <geoff@geoffgarside.co.uk>
2013-04-15 11:47:49 +0100

    Add mocha and schema loading to test/helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,19 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'mocha/setup'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'epp-client'
 
 class Test::Unit::TestCase
+  def load_schema(name)
+    xsd_path = File.expand_path("../schemas/#{name}.xsd", __FILE__)
+    xsd_doc  = XML::Document.file(xsd_path)
+    XML::Schema.document(xsd_doc)
+  end
+  def schema
+    @schema ||= load_schema('all')
+  end
 end
