561d98a0a43a1501d6bb8a1c2fbed4df724fc916
Geoff Garside <geoff@geoffgarside.co.uk>
2013-08-21 08:55:50 +0100

    Remove replaced Request subclasses

--- lib/epp-client/requests/hello_request.rb
+++
@@ -1,9 +0,0 @@
-module EPP
-  # An EPP Hello Request
-  class HelloRequest < Request
-    # Create new HelloRequest instance
-    def initialize(*ignored)
-      xml.root << xml_node('hello')
-    end
-  end
-end
