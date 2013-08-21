39bef4bb76d68b6b057b6de97f3f139c056a3173
Geoff Garside <geoff@geoffgarside.co.uk>
2013-08-21 09:13:41 +0100

    Update EPP::Client tests for new properties

--- test/test_client.rb
+++ test/test_client.rb
@@ -3,50 +3,52 @@ require 'helper'
 class TestEppClient < Test::Unit::TestCase
   context 'EPP::Client' do
     setup do
-      stub_getaddrinfo!
-
       @client = EPP::Client.new('TEST', 'test', 'epp.test.host')
     end
-
+  
     should 'be instance of EPP::Client' do
       assert @client.kind_of?(EPP::Client)
     end
-
+  
     should 'allow configuration of port' do
-      stub_getaddrinfo!(nil, 7001)
-
       client = nil
       assert_nothing_raised do
         client = EPP::Client.new('TEST', 'test', 'epp.test.host', :port => 7001)
       end
-
-      assert_equal 7001, client.instance_variable_get("@conn").options[:port]
+  
+      assert_equal 7001, client.options[:port]
     end
-
+  
     should 'allow configuration of language' do
       client = EPP::Client.new('TEST', 'test', 'epp.test.host', :lang => 'no')
-
-      assert_equal 'no', client.instance_variable_get("@conn").options[:lang]
+  
+      assert_equal 'no', client.options[:lang]
     end
-
+  
     should 'allow configuration of version' do
       client = EPP::Client.new('TEST', 'test', 'epp.test.host', :version => '2.0')
-
-      assert_equal '2.0', client.instance_variable_get("@conn").options[:version]
+  
+      assert_equal '2.0', client.options[:version]
     end
-
+  
     should 'allow configuration of extensions' do
       extensions = %w(urn:ietf:params:xml:ns:secDNS-1.1)
       client = EPP::Client.new('TEST', 'test', 'epp.test.host', :extensions => extensions)
-
-      assert_equal extensions, client.instance_variable_get("@conn").options[:extensions]
+  
+      assert_equal extensions, client.options[:extensions]
     end
-
+  
     should 'allow configuration of services' do
       services = %w(http://www.nominet.org.uk/epp/xml/nom-domain-2.0)
       client = EPP::Client.new('TEST', 'test', 'epp.test.host', :services => services)
+  
+      assert_equal services, client.options[:services]
+    end
 
-      assert_equal services, client.instance_variable_get("@conn").options[:services]
+    should 'allow compatibility mode' do
+      client = EPP::Client.new('TEST', 'test', 'epp.test.host', :compatibility => true)
+
+      assert client.compatibility?
     end
   end
 end
