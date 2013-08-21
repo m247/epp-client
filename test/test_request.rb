e455f94c08223645ed90542670079e16ed01eab1
Geoff Garside <geoff@geoffgarside.co.uk>
2013-08-21 08:42:07 +0100

    Add tests for EPP::Request

--- test/test_request.rb
+++ test/test_request.rb
@@ -3,18 +3,8 @@ require 'helper'
 class TestEppRequest < Test::Unit::TestCase
   context 'EPP::Request' do
     setup do
-      @request = EPP::Request.new('info', 'TESTING-123') do |cmd, ext|
-        info = XML::Node.new('info')
-        info.namespaces.namespace = ns = XML::Namespace.new(info, 'domain', 'urn:ietf:params:xml:ns:domain-1.0')
-
-        info << XML::Node.new('name', 'testing.com', ns)
-
-        cmd << info
-      end
-    end
-
-    should 'be info command' do
-      assert_equal 'info', @request.command
+      hello   = EPP::Requests::Hello.new
+      @request = EPP::Request.new(hello)
     end
 
     should 'validate against schema' do
