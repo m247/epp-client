61b68b0f449ec72a329cdadd0d1a04f8c462b5ed
Geoff Garside <geoff@geoffgarside.co.uk>
2013-09-11 15:37:33 +0100

    Support multiple extensions in Extension block

--- lib/epp-client/requests/extension.rb
+++ lib/epp-client/requests/extension.rb
@@ -3,8 +3,8 @@ require File.expand_path('../abstract', __FILE__)
 module EPP
   module Requests
     class Extension < Abstract
-      def initialize(extension)
-        @extension = extension
+      def initialize(*extensions)
+        @extensions = extensions
       end
 
       def name
@@ -14,8 +14,12 @@ module EPP
       def to_xml
         node = super
 
-        @extension.set_namespaces(@namespaces) if @extension.respond_to?(:set_namespaces)
-        node << as_xml(@extension)
+        Array(@extensions).each do |extension|
+          next if extension.nil?
+          extension.set_namespaces(@namespaces) if extension.respond_to?(:set_namespaces)
+          node << as_xml(extension)
+        end
+
         node
       end
     end
--- test/requests/test_extension_request.rb
+++ test/requests/test_extension_request.rb
@@ -2,23 +2,54 @@ require 'helper'
 
 class TestEppExtensionRequest < Test::Unit::TestCase
   context 'EPP::Requests::Extension' do
-    setup do
-      @domain_info  = XML::Node.new('info')
-      @domain_info.namespaces.namespace = ns =
-        XML::Namespace.new(@domain_info, 'domain', "urn:ietf:params:xml:ns:domain-1.0")
+    context 'single extension' do
+      setup do
+        @domain_info  = XML::Node.new('info')
+        @domain_info.namespaces.namespace = ns =
+          XML::Namespace.new(@domain_info, 'domain', "urn:ietf:params:xml:ns:domain-1.0")
 
-      xattr = XML::Attr.new(@domain_info, "schemaLocation", "urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd")
-      xattr.namespaces.namespace = XML::Namespace.new(@domain_info, 'xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+        xattr = XML::Attr.new(@domain_info, "schemaLocation", "urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd")
+        xattr.namespaces.namespace = XML::Namespace.new(@domain_info, 'xsi', 'http://www.w3.org/2001/XMLSchema-instance')
 
-      @domain_info << XML::Node.new('name', 'example.com', ns)
+        @domain_info << XML::Node.new('name', 'example.com', ns)
 
-      @extension = EPP::Requests::Extension.new(@domain_info)
-      @request   = EPP::Request.new(@extension)
+        @extension = EPP::Requests::Extension.new(@domain_info)
+        @request   = EPP::Request.new(@extension)
+      end
+
+      should 'validate against schema' do
+        xml = @request.to_xml
+        assert xml.validate_schema(schema), xml.to_s(:indent => 2)
+      end
     end
+    context 'mutltiple extensions' do
+      setup do
+        @domain_info  = XML::Node.new('info')
+        @domain_info.namespaces.namespace = ns =
+          XML::Namespace.new(@domain_info, 'domain', "urn:ietf:params:xml:ns:domain-1.0")
 
-    should 'validate against schema' do
-      xml = @request.to_xml
-      assert xml.validate_schema(schema), xml.to_s(:indent => 2)
+        xattr = XML::Attr.new(@domain_info, "schemaLocation", "urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd")
+        xattr.namespaces.namespace = XML::Namespace.new(@domain_info, 'xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+
+        @domain_info << XML::Node.new('name', 'example.com', ns)
+
+        @domain_check = XML::Node.new('check')
+        @domain_check.namespaces.namespace = ns =
+          XML::Namespace.new(@domain_check, 'domain', "urn:ietf:params:xml:ns:domain-1.0")
+
+        xattr = XML::Attr.new(@domain_check, "schemaLocation", "urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd")
+        xattr.namespaces.namespace = XML::Namespace.new(@domain_check, 'xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+
+        @domain_check << XML::Node.new('name', 'example.com', ns)
+
+        @extension = EPP::Requests::Extension.new(@domain_info, @domain_check)
+        @request   = EPP::Request.new(@extension)
+      end
+
+      should 'validate against schema' do
+        xml = @request.to_xml
+        assert xml.validate_schema(schema), xml.to_s(:indent => 2)
+      end
     end
   end
 end
