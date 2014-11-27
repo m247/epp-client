463c50b23a8fb43ba88a7900826ac4a00c2838f5
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-27 11:15:49 +0000

    Update/fix documentation of Domain::Create.

--- lib/epp-client/domain/create.rb
+++ lib/epp-client/domain/create.rb
@@ -3,17 +3,35 @@ require File.expand_path('../command', __FILE__)
 module EPP
   module Domain
     class Create < Command
+      # Domain creation command.
       #
-      # @param [String] name
-      # @param [String] period
-      # @param [Array<String,Hash>] nameservers
-      # @param [String] registrant
-      # @param [Hash<Symbol,String>] contacts
-      # @param [Hash<Symbol,String>] auth_info
+      # @param [String] name Name of domain to create
+      # @param [Hash] options Options to use to create the domain
+      # @option options [String] period Years or months period to create the domain for
+      # @option options [Array<String,Hash>] Array or hash of nameservers
+      # @option options [String] registrant Contact handle to use as registrant
+      # @option options [Hash<Symbol,String>] contacts Hash of admin, tech, billing contacts
+      # @option options [Hash<Symbol,String>] auth_info Hash of auth info
+      # @return [EPP::Domain::Create]
       #
-      # @option contacts [String] :admin
-      # @option contacts [String] :tech
-      # @option contacts [String] :billing
+      # @example Create a domain
+      #   command = EPP::Domain::Create.new('example.com',
+      #     period: '1y', registrant: 'test9023742684',
+      #     auth_info: { pw: 'domainpassword' },
+      #     contacts: { admin: 'admin123', tech: 'admin123', billing: 'admin123' },
+      #     nameservers: ['ns1.test.host', 'ns2.test.host']
+      #   )
+      #
+      # @example Create a domain with nameserver glue
+      #   command = EPP::Domain::Create.new('example.com',
+      #     period: '1y', registrant: 'test9023742684',
+      #     auth_info: { pw: 'domainpassword' },
+      #     contacts: { admin: 'admin123', tech: 'admin123', billing: 'admin123' },
+      #     nameservers: [
+      #       {name: 'ns1.example.com', ipv4: '198.51.100.53'}
+      #       {name: 'ns2.example.com', ipv4: '198.51.100.54'}
+      #     ]
+      #   )
       def initialize(name, options = {})
         @name = name
         @period = options.delete(:period) || '1y'
--- lib/epp-client/domain/create_response.rb
+++ lib/epp-client/domain/create_response.rb
@@ -3,14 +3,23 @@ require File.expand_path('../response', __FILE__)
 module EPP
   module Domain
     class CreateResponse < Response
+      # Return the name of the domain created if successful
+      #
+      # @return [String] Name of created domain
       def name
         return nil unless success?
         @name ||= value_for_xpath('//domain:name')
       end
+      # Return the creation/registration date of the domain if the request was successful.
+      #
+      # @return [Time] Registration time
       def creation_date
         return nil unless success?
         @crdate ||= value_for_xpath('//domain:crDate') && Time.parse(value_for_xpath('//domain:crDate'))
       end
+      # Return the expiration date of the domain if the request was successful.
+      #
+      # @return [Time] Expiration time
       def expiration_date
         return nil unless success?
         @exdate ||= value_for_xpath('//domain:exDate') && Time.parse(value_for_xpath('//domain:exDate'))
